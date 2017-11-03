92b6168bfa2c7d427779bffdadac78537ff36fe1
Elise Ziemer <264665990@myunit5.org>
2017-11-03 19:45:04 +0000

    Create that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -4,13 +4,3 @@ function sayThatWasEasy() {
 }
 
 $("#easy").on("click", sayThatWasEasy);
-
-#easy {
-    width: 300px;
-    height: 300px;
-    border-radius: 150px;
-    border-color: black;
-    background-color: red;
-    font-size: 50px;
-    color: white;
-}
